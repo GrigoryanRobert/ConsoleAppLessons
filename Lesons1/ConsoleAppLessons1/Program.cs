0c5cd18e1db210cf6d7a530b6cf4e531e05c9fc8
GrigoryanRobert <youngman87@mail.ru>
2023-02-10 22:51:09 +0300

    Update Program.cs

--- Lesons1/ConsoleAppLessons1/Program.cs
+++ Lesons1/ConsoleAppLessons1/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace ConsoleAppLessons1
+{
+    class TestClass
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 4;
+            if (a > b)
+            {
+                Console.WriteLine("A big than B");
+            }
+            else
+            {
+                Console.WriteLine("B big than A");
+            }
+
+            Console.WriteLine(a > b ? "A big than B" : "B big than A");
+        }
+    }
+
+}
